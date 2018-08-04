d45bca4173cd30801985946a4ff7628ddf4add7b
ciatph <ciat.ph@gmail.com>
2018-08-05 04:13:30 +0800

    Connected user registration to backend. Used PNotify for notification alerts.

--- public/js/scripts/main.js
+++ public/js/scripts/main.js
@@ -13,10 +13,77 @@ var App = function(){
     this.reg_email = document.getElementById('reg_email');
     this.reg_pass = document.getElementById('reg_password');        
 
+    // Swap UI buttons
     this.btn_reg.addEventListener('click', this.swapInterface.bind(this));
     this.btn_login.addEventListener('click', this.swapInterface.bind(this));
+
+    // Submit buttons
+    this.btn_submit_reg = document.getElementById('reg_submit');
+    this.btn_submit_login = document.getElementById('reg_submit');
+
+    this.btn_submit_reg.addEventListener('click', this.registerEmailPassword.bind(this));
 };
 
+
+
+/**
+ * Register newly-created accounts to the guestbook. 
+ * Full access for other nodes is pending for admin approval
+ */
+App.prototype.registerEmailPassword = function(e){
+	console.log("clicked register");
+	var input = {
+		email: this.reg_email.value,
+		fname: this.reg_fname.value,
+		lname: this.reg_lname.value,
+		date_created: firebase.database.ServerValue.TIMESTAMP,
+		date_approved: "",
+		acct_status: "3",
+		acct_type: "1",
+		login_stat: ""
+	};
+
+	firebase.auth().createUserWithEmailAndPassword(input.email, this.reg_pass.value).then(function(user){
+		var user = firebase.auth().currentUser;
+		console.log("User created, success! " + user.email + ", uid: " + user.uid + "\n" +
+			input.email + ", " + input.lname);
+
+		new PNotify({
+			title: "New account " + user.email + " was created.",
+			text: "Please wait for admin approval.",
+			type: 'success',
+			styling: 'bootstrap3'
+		});		
+
+		// Insert registration data to firebase
+		firebase.database().ref('guestbook/' + user.uid).set(input);
+		firebase.database().ref('guestbook_names/' + user.uid).set({
+			"fname": input.fname,
+			"lname": input.lname,
+			"email": input.email,
+			"date_created": firebase.database.ServerValue.TIMESTAMP
+		});
+	}, function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+
+		new PNotify({
+			title: errorCode,
+			text: errorMessage,
+			type: 'success',
+			styling: 'bootstrap3'
+		});		
+		console.log(errorCode + "\n" + errorMessage);
+	}).catch(function(error){
+		new PNotify({
+			title: "Something went wrong",
+			text: error,
+			type: 'danger',
+			styling: 'bootstrap3'
+		});		
+	});
+};
 
 
 /**
@@ -26,6 +93,10 @@ App.prototype.initialize = function(){
 };
 
 
+/**
+ * Toggle the login/registration UI
+ * @param {*} e 
+ */
 App.prototype.swapInterface = function(e){
     if(e.target.id === 'signup'){
         console.log('create new account');
---
+++ public/js/scripts/utils.js
@@ -0,0 +1,26 @@
+var Utils = function(){
+
+};
+
+
+Utils.prototype.getCurrentDate = function(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    
+    var yyyy = today.getFullYear();
+    if(dd < 10){
+        dd = '0' + dd;
+    } 
+    if(mm < 10){
+        mm = '0' + mm;
+    } 
+    
+    var today = dd+'/'+mm+'/'+yyyy;
+    return today;
+};
+
+
+window.onload = function(){
+    window.Utils = new Utils();
+};
